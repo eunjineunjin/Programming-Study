d204038cd044ac8f5211301b5efb3a0cdc8b97eb
Lee Chang Ha <yielding@gmail.com>
2020-02-27 21:32:38 +0900

    c++ implementation of stack (still has a lot todo)

---
+++ C/stack_array_int.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+class Stack
+{
+public:
+  Stack(int limit=30)
+  {
+    m_top = -1;
+    m_max = limit;
+  }
+
+  auto push(int item) -> Stack& 
+  {
+    if (is_full())
+      cout << "stack is full\n";
+    else
+      m_arr[++m_top] = item;
+
+    return *this;
+  }
+
+  auto pop() -> void
+  {
+    if (is_empty())
+    {
+      cout << "stack is empty\n";
+      return;
+    }
+
+		m_top--;
+  }
+
+  auto peek() -> int
+  {
+		return m_arr[m_top];
+  }
+
+  auto top() -> int
+  {
+		return peek();
+  }
+
+  auto is_full() const -> bool 
+  {
+    return m_top == m_max;
+  }
+
+  auto is_empty() const -> bool
+  {
+    return m_top == -1;
+  }
+
+private:
+  const static int MAX_SIZE = 100;
+  int m_arr[MAX_SIZE];
+  int m_top {-1};
+  int m_max{MAX_SIZE};
+};
+
+int main()
+{
+	Stack s;
+  s.push(1).push(2).push(3);
+  s.pop();
+
+  cout << s.top();
+
+  return 0;
+}
